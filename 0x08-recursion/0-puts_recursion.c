b3b4868685855313901d8e741fe4c1ddf4cf4229
youssef <yusufkhattab3@gmail.com>
2023-04-05 12:35:06 -0700

    second

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * this will print a string
+ * and return nothing if met with a \0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
