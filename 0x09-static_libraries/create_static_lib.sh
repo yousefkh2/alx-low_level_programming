bef6a852827b84f4aa5f3a76659a7a9a93903a81
youssef <yusufkhattab3@gmail.com>
2023-04-09 14:36:09 -0700

    last

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
-gcc -c *.c
-ar -rcs liball.a *.o
+#include "main.h"
+
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
+
