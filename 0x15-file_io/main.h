3e4f41a8719c9dbb82e76f133ad39024d7da4a77
Yousef <yusufkhattab3@gmail.com>
2023-05-27 15:00:15 +0300

    final 3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,11 @@
 
 #include <stddef.h>
 #include <sys/types.h>
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define BUF_SIZE 1024
 
 ssize_t _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
