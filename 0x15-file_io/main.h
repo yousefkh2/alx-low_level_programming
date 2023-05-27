919cf076de25f821757b6dd3abf0f229f49ee235
Yousef <yusufkhattab3@gmail.com>
2023-05-27 15:01:30 +0300

    final 4

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,18 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stddef.h>
-#include <sys/types.h>
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
-#define BUF_SIZE 1024
+#include <string.h>
 
-ssize_t _putchar(char c);
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error_98(int f0, char *buffer, char *argv);
+void error_99(int f0, char *buffer, char *argv);
+void error_100(int f0, char *buffer);
 
-#endif /* MAIN_H */
-
+#endif/**MAIN_H*/
